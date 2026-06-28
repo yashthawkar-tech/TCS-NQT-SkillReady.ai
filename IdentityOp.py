c09136b7eb9c7d7e3acca8920fc90e931b3d4f14
Yash Thawkar <yashthawkar595@gmail.com>
2026-06-28 11:46:54 +0530

    IdentityOp.py
    Added additional identity operator examples for different data types.

--- IdentityOp.py
+++ IdentityOp.py
@@ -10,3 +10,11 @@ print (a is b)
 a =[1 ,2 ,3]
 b =[1,2,3]
 print(a is b)#memory location differnt Thats why it is false
+
+a = 12.5
+b =12.5
+print(a is b)
+
+a = [2,4,6]
+b = [2,4,6]
+print( a is not b) # is not will not compare values 
