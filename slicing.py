7b75404772146a746913dd00dc7c2ad87cff93b6
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-01 20:56:03 +0530

    Day 5 - Python Tuples and Type Conversion
    Uploaded Python programs demonstrating tuple creation, tuple to list conversion, list to tuple conversion, and adding elements to a tuple using list conversion as part of my TCS NQT and Python learning journey.

---
+++ slicing.py
@@ -0,0 +1,10 @@
+#tuple slicing
+tuple = (10,20,30,40,50)
+#        0  1  2  3  4
+print(tuple[1:4])#it will print 2nd to 4th element of the tuple
+print(tuple[0:4])#it will print 1st to 4th element of the tuple
+print(tuple[2:])#it will print 3rd to last element of the tuple
+print(tuple[:4])#it will print 1st to 4th element of the tuple
+print(tuple[:-1])#it will print 1st to 4th element of the tuple
+
+
---
+++ traversing.py
@@ -0,0 +1,7 @@
+#traversing tuple
+import time
+tuple = (10,20,30,40,50)
+print(tuple)
+for i in tuple:
+    print(i)
+    time.sleep(2)#This will print each element of the tuple after 2 seconds
