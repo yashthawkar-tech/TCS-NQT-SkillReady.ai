9c85fbee36224e606e26ca3ec4f01714d0954b59
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-08 20:12:36 +0530

    Basic_syntax_of_cpp

---
+++ basic_example.cpp
@@ -0,0 +1,32 @@
+// C & C++
+
+// What is Header File?==>stdio.h
+// library ==>Multiple Functions Classes 
+
+// Which Location are Stored the stdio.h 
+// MinGw Include Folder
+
+// What is the use of # Symbol
+//It gives instruction to compiler to include the header file in the program
+
+// What is the use of main method Function?
+// It is Entry Gate of Compiler
+
+// #==> compiler to provide instruction
+
+// Why we are Using f in printf?
+
+//<>Why we use this for header File?
+//For Specific Searching of file stdio ,Not any similar files 
+
+//; Semicolon ; Line End 
+
+//{} ==> Define Krne ke Liye 
+
+// void keyword only uses in C Files
+
+#include<stdio.h>
+int main()
+{// main is Entry Gate for Compiler
+    printf("Hello World!");// f means format string
+}
