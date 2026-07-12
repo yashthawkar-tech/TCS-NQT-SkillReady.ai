de2fc17b421a666b0d06d40b471fd330afbf00e8
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-12 11:37:20 +0530

    practise-For_Loop1
    Solved the practise questions using for loop

---
+++ practise_for_loop1.py
@@ -0,0 +1,141 @@
+# Task 9: Find Sum of Digits
+# Problem: Find the sum of digits of a number.
+# Input:
+# 1234
+# Output:
+# 10
+# n =int(input("Enter Any Number:"))
+# sum=0
+# for i in range(1,n+1):
+#     sum = sum+i
+# print(sum)
+
+# Task 10: Reverse a Number
+# Problem: Reverse the digits of a number.
+# Input:
+# 12345
+# Output:
+# 54321
+
+# Task 11: Print All Factors
+# Problem: Print all factors of a given number.
+# Input:
+# 12
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 6
+# 12
+
+# Task 12: Check Prime Number
+# Problem: Check whether a number is Prime or Not.
+# Input:
+# 17
+# Output:
+# Prime Number
+
+
+# Task 13: Count Vowels in a String
+# Problem: Count the number of vowels in a given string.
+# Input:
+# education
+# Output:
+# 5
+
+
+# Task 14: Print Each Character of a String
+# Problem: Print each character of a string on a new line.
+# Input:
+# Python
+# Output:
+# P
+# y
+# t
+# h
+# o
+# n
+# n =input("Enter a String:")
+# for i in n:
+#     print(i)
+
+# Task 15: Find Sum of List Elements
+# Problem: Take a list of numbers and find their sum using a for loop.
+# Input:
+# [10, 20, 30, 40]
+# Output:
+# 100
+# list=[10,20,30,40]
+# sum=0
+# for i in list:
+#     sum=sum+i
+# print(sum)
+
+# Task 16: Find Largest Number in a List
+# Problem: Find the largest element in a list.
+# Input:
+# [12, 45, 78, 23, 56]
+# Output:
+# 78
+# list=[12,45,98,23,56]
+# large=0
+# for i in list:
+#     if large<i:
+#      large=i
+# print(large)
+
+# Task 17: Count Even and Odd Numbers
+# Problem: Count the number of even and odd elements in a list.
+# Input:
+# [2, 5, 8, 9, 10]
+# Output:
+# Even = 3
+# Odd = 2
+# list=[2,5,8,9,10]
+# even=0
+# odd=0
+# for i in list:
+#    if i%2==0:
+#       even=even+1
+#    else:
+#       odd=odd+1
+# print("Even Number in the list:",even,"\nOdd Number in the list:",odd)
+
+# Task 18: Print Star Pattern
+# Problem: Print the following pattern using a for loop.
+# Input:
+# 5
+# Output:
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# Task 19: Print Number Pattern
+# Problem: Print the following pattern.
+# Input:
+# 5
+# Output:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# Task 20: Fibonacci Series
+# Problem: Print the first N terms of the Fibonacci series.
+# Input:
+# 7
+# Output:
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
+
+
