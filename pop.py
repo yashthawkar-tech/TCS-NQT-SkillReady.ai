3f9b48bef106469a7a399b5f0cf56641d901afd6
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-21 20:08:42 +0530

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,40 @@
+#pop()==> remove the random element from set
+#set={} dict={key:value}
+# number = {10,20,30,40,50}
+# print(number.pop())
+# data =number.pop()
+# print(data)
+# print(number)
+
+
+#Hardcode Logic
+# number = {10,20,30,40,50}
+# temp=list(number)
+# print(temp)
+# data = temp[0]
+# number.remove(data)
+# print(data)
+# print(number)
+
+
+#hardcode logic for string 
+# num2={"Tv","Mobile","mouse","keyboard"}
+# data = None
+# for i in num2:
+#     data =i
+#     break
+# num2.remove(data)
+# print(data)
+# print(num2)
+
+num3={"Tv","Mobile","mouse","keyboard"}
+removed = None
+new_set=set()
+for i in num3:
+    if removed is None:
+        removed =i
+    else:
+        new_set.add(i)
+num3=new_set
+print(removed)
+print(num3)
