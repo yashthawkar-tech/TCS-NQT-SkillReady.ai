8b0628a22243ac9fb2d165a2ab86e8704cfc929b
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-21 20:08:27 +0530

    Add files via upload

---
+++ discard.py
@@ -0,0 +1,18 @@
+#Remove the elements if elements are not available 
+#then show the error 
+#does not provide the error
+# color={"red","blue","green"}
+# color.discard("red") #It will discard this color
+# print(color)
+
+
+#Hardcode Logic for remove function
+color=["red","blue","green"]
+rmv="red"
+list=[]
+for i in color:
+    if i !=rmv:
+        list.append(i)
+print(list)
+
+#Task ==> Write down the hardcode logic for discard
