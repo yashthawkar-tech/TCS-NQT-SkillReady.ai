7738a462b34948b78c2363ebf4262ed2bfc15ff8
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-22 20:07:38 +0530

    mathematical-op-in-sets

---
+++ mathematicalOp.py
@@ -0,0 +1,12 @@
+#union()
+#intersection()
+# difference()
+# symmetric_difference()
+# intersection_update()
+# difference_update()
+#symmetric_difference_update()
+
+#relationship mehthod
+# issubset
+# issuperset
+# isdisjoint
