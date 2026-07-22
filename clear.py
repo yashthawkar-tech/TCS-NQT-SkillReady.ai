0ca4ee9ae8f51e998f086d708a2fe7c24a5d8aba
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-22 20:07:03 +0530

    clear-function-in-sets

---
+++ clear.py
@@ -0,0 +1,13 @@
+number={10,20,30,40,50}
+number.clear()#It will clear all elements in the set
+print(number)
+
+#hardcode logic
+
+#copy function
+a={1,2,3,4,5}
+b = a.copy()
+print(a)
+print(b)
+
+#hardcode logic Easy way
