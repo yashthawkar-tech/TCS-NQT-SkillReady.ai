da28017e4fe3260b6f9925e3c054af02c4cfcf43
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-23 20:31:20 +0530

    difference2

---
+++ difference2.py
@@ -0,0 +1,11 @@
+a={1,2,3,4}
+b={3,4,5}
+c=a.difference(b)
+print("Value of C:",c)
+print("value of A:",a)
+#It will Not modify the original set
+
+
+a.difference_update(b)
+print(a)
+#It will update the original set
