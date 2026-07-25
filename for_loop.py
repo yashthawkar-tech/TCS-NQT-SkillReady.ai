2d4bd183f4bbbc2c2deedd05bf911bf962fa2e4f
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-25 23:21:59 +0530

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+# #print 1 to 10
+# for i in range(1,11):
+#     print(i)
+
+# #Print it Without Range
+# num="123456789"
+# for i in num:
+#     print(i)
+
+#print Reverse 10 to 1
+# for i in range(10,0,-1):
+#     print(i)
