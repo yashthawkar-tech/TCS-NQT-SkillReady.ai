84a99b2b383cd884a42fd1b5a5c724d18b8a2717
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-25 23:22:34 +0530

    while_loop

---
+++ while_loop.py
@@ -0,0 +1,61 @@
+#print 1 to 10 using while
+# i=0
+# while i <= 10:
+#     print(i)
+#     i+=1
+
+
+#print 10 to 1 using while
+
+# i=10
+# while i >=1:
+#     print(i)
+#     i-=1
+
+# #print Even Numbers from 1 to 20
+# i =1
+# while i<=20:
+#     if i%2 ==0:
+#         print(i)
+#     i+=1
+
+# #print Even Numbers from 20 to 1 in Reverse
+# i =20
+# while i>=1:
+#     if i%2 ==0:
+#         print(i)
+#     i-=1
+
+#Add the 1 to 15 numbers 1+2+3...+15
+# i =1
+# count=0
+# while i<=15:
+#     # print(i)
+#     count=count+i
+#     i+=1
+# print(count)
+
+
+#multiplication table any number
+# a=int(input("Enter Any Number:"))
+# i=1
+# mul=0
+# while i<=10:
+#     mul = a*i
+#     print(a,"x",i,"=",mul)
+#     i +=1
+
+
+#Take user input and check it is a pos or neg number
+a=int(input("Enter any Number:"))
+i =1
+while i<=10:
+    if a<0:
+        print("Negetive Number")
+        break
+    else:
+        print("Positive Number")
+        break
+
+list=int(input())
+
