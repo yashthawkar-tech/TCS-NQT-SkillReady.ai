4f60bad2510ea9f28dc1b9b4f0c11f84f5958492
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-27 21:41:10 +0530

    Fix comment formatting in slicing.py

--- slicing.py
+++ slicing.py
@@ -1,6 +1,6 @@
 #tuple slicing
 tuple = (1,2,3,4,5)
-#        0  1  2  3  4
+#        0 1 2 3 4
 print(tuple[1:4])#it will print 2nd to 4th element of the tuple
 print(tuple[0:4])#it will print 1st to 4th element of the tuple
 print(tuple[2:])#it will print 3rd to last element of the tuple
