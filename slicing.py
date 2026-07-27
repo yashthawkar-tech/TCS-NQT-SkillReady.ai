f0e5a92dc4fc3bb3c07ce02d6f6d485ade0722b3
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-27 21:40:42 +0530

    Change tuple values in slicing.py

--- slicing.py
+++ slicing.py
@@ -1,5 +1,5 @@
 #tuple slicing
-tuple = (10,20,30,40,50)
+tuple = (1,2,3,4,5)
 #        0  1  2  3  4
 print(tuple[1:4])#it will print 2nd to 4th element of the tuple
 print(tuple[0:4])#it will print 1st to 4th element of the tuple
