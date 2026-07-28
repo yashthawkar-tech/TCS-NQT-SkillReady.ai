88d969aa339d4fa7d07d63efc96c80f2a2cc476b
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-28 21:37:51 +0530

    Modify comment for number set in pop.py
    Updated the comment to reflect the new range of numbers.

--- pop.py
+++ pop.py
@@ -1,6 +1,6 @@
 #pop()==> remove the random element from set
 #set={} dict={key:value}
-# number = {10,20,30,40,50}
+# number = {10,20,30,40,50,60,70,80,90,100}
 # print(number.pop())
 # data =number.pop()
 # print(data)
