d0df5f5566ace25370c7d5ff26b430ed3eba71b0
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-28 21:39:09 +0530

    Add 10 to the tuple in sum.py

--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
-tuple=(1,2,3,4,5,6,7,8,9)
+tuple=(1,2,3,4,5,6,7,8,9,10)
 print(sum(tuple))#sum() function is used to find the sum of all values in tuple
 
 #hard coded logic for sum function in tuple
