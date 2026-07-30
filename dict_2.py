fdd3c93ac7ac72471a17238e60a6d642fa81cf6f
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-30 20:05:40 +0530

    Add files via upload

---
+++ dict_2.py
@@ -0,0 +1,11 @@
+student={
+    "name":"Aakash",
+    "age" :28,
+    "city" :"Tirora"
+}
+print(student["name"])
+print(student["age"])
+print(student["city"])
+
+#it will print a key Value Assigned to that Particular Variable
+
