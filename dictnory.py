871d46cceec3dce6c48e17e6c71bcb7d03e2630d
Yash Thawkar <yashthawkar595@gmail.com>
2026-07-30 20:01:50 +0530

    dictnory

---
+++ dictnory.py
@@ -0,0 +1,10 @@
+#Key Value Pair
+#can store Multiple Type of Data
+#Syntax==> var_name={Key:Value}
+student ={
+    "name":"yash",
+    "age" : 28,
+    "course":"Data Analytics Course"
+}
+print(student)
+print(type(student))
