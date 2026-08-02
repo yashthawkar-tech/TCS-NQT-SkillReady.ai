6541cc53bd85bccf2bc9f9b6cd49849aa297b035
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-02 20:34:00 +0530

    Add elements 7 to 10 to the data list

--- list2.py
+++ list2.py
@@ -1,6 +1,6 @@
 #access last element
 
-data = [1,2,3,4,5,6]
+data = [1,2,3,4,5,6,7,8,9,10]
 print(data[-1])#it will return the last element (it starts from the end)
 print(data[:])#print all values 
 print(data[::-1])#it will reverse the list
