668dae996d1a03653d3488958b2332d8a66b6b49
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-03 21:04:40 +0530

    Zero-Exception

---
+++ zero.py
@@ -0,0 +1,12 @@
+#exception ==> abnormal condition
+#not solved or solution are not available
+#error == exception not equal
+#try except
+
+
+a=10
+b=0
+print(a/b)
+# Zero Division Error (we call it as Exception)
+
+
