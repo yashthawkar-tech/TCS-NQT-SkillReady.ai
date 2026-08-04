bf4166fd3ff5ff5e1f00480b5c39297bf1a01799
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-04 20:28:02 +0530

    name_error

---
+++ name_error.py
@@ -0,0 +1,10 @@
+# try :
+#     print(age)
+# except NameError:
+#     print("please declare the variable")
+
+# try:
+#     name="yash"
+#     print(namme)
+# except Exception:
+#     print("Please correct it")
