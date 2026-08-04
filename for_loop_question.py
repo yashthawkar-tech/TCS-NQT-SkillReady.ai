a55a28cdb8d35148dca8cacd0015bd3b3e4a9ea8
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-04 20:29:14 +0530

    for_loop_question

---
+++ for_loop_question.py
@@ -0,0 +1,12 @@
+#Without 2nd or 3rd variable print the 
+#starting of 10 numbers addition using for loop
+#using for loop 1 to 10 number print
+#addition =print 55 only single variable
+#2nd or 3rd variable declared
+
+
+for i in range(1,11):
+    print(i)
+    if i ==10:
+        i=10 * (10+1)//2
+        print("Addition =",i)
