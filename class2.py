4ca11c599a476a663ea010f89b473515ef08c4d6
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-05 20:16:23 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,9 @@
+class Data:
+    def __init__(self):#init==> Constructer
+        print("This is init function")
+    def printdata(self):
+         print("printdata_function")
+      
+
+s1=Data()
+s1.printdata()#It will be use to access That Particular Data
