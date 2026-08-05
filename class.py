e65931934575efa56ad4d7020889801fd7a11e10
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-05 20:14:34 +0530

    OOPs-Concept

---
+++ class.py
@@ -0,0 +1,8 @@
+#how to declare the class
+class student:
+    def print1(self):
+        print("print function")
+
+s1=student()#object declaration 
+#We can access class data using object
+s1.print1()
