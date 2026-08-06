d9dc483e0b2c94f6f76514b9996b166fb2f0b2fd
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-06 20:00:45 +0530

    class4

---
+++ class4.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self):
+        self.name=input("enter your name:")
+        self.age=int(input("enter your age:"))
+        self.mobilenumber=int(input("Enter Your Mobile Number:"))
+        print("\nStudent Details")
+        print("Name:",self.name)
+        print("Age:",self.age)
+        print("Age:",self.mobilenumber)
+s1=student()
