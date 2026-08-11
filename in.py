fd3b718cc447f38577705ae8c62bc9d7090f8b96
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-11 23:44:26 +0530

    Add pincode to student dictionary

--- in.py
+++ in.py
@@ -2,7 +2,8 @@ student={
     "name":"Yash",
     "age":22,
     "city":"Nagpur",
-    "branch":"RAI"
+    "branch":"RAI",
+    "pincode":441911
 }
 print("name" in student)
 print("salary" not in student)
