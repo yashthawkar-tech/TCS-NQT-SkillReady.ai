0012a6b2a18bbd312c68a1d424bdae3b27695fd8
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-16 16:05:07 +0530

    Polymorphism_run_time

---
+++ runtime.py
@@ -0,0 +1,21 @@
+#Method overriding
+class calculator:
+    def calculate(self,a,b):
+        print("Addition=",a+b)
+class AdvanceCalculator(calculator):
+    def calculate(self,a,b):
+        print("Division =",a/b)
+object1=calculator()
+object2=AdvanceCalculator()
+
+object2.calculate(10,20)
+
+#Mantrimall / Dream 11 
+#This Method call is overriding
+
+#Task Using Polymorphism 
+#Print The Reverse Number
+#Pallindrome Number
+#Largest Number in given Number
+#Second Largest number in given number
+
