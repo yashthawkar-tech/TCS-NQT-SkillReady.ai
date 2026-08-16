e6c16bb15b2b45876dc71edfa5c3ad1a40493a67
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-16 16:04:35 +0530

    Polymorphism_compile_time

---
+++ compiletime.py
@@ -0,0 +1,34 @@
+#Compile time Polymorphism means
+#method overloading
+#One Person ==> But different Reaction
+#shopkeeper:Customer's
+            #child kids
+            #Youngsters
+            #Old age
+class calculator:
+    def add(self,*a):#For Multiple arguments
+        print(sum(a))
+    # def add(self,a,b):
+    #     print(a+b)
+    # def add(self,a,b,c):
+    #     print(a+b+c)
+object=calculator()
+object.add(10,20,30,40,50,60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
