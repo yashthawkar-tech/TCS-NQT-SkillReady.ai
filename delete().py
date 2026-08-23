141382be7df3f14e3bf52a260b3d9a3ee9e0604b
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-23 20:34:41 +0530

    Modify student dictionary by deleting 'city' and adding 'Year'
    Added 'Year' key to student dictionary and deleted 'city'.

--- delete().py
+++ delete().py
@@ -3,7 +3,8 @@ student={
     "name":"Yash",
     "age":22,
     "city":"Nagpur",
-    "branch":"RAI"
+    "branch":"RAI",
+    "Year":"4th Year"
 }
 del student["city"]#It will delete (use [] list bracket)
 print(student)
