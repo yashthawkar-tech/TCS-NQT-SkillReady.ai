7ead59cc68343a991180eabe6df358c6680d5c7f
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-25 23:33:41 +0530

    Fix syntax error in student dictionary

--- in.py
+++ in.py
@@ -2,8 +2,7 @@ student={
     "name":"Yash",
     "age":22,
     "city":"Nagpur",
-    "branch":"RAI",
-    "pincode":441911
+    "branch":"RAI"
 }
 print("name" in student)
 print("salary" not in student)
