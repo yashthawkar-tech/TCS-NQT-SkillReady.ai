1d44380149c52c2950c88858924d92b2ed90353d
Yash Thawkar <yashthawkar595@gmail.com>
2026-08-27 20:26:06 +0530

    Two-sum.py

---
+++ List_twosum.py
@@ -0,0 +1,13 @@
+# list=[2,3,4,5,7,8]
+# target=10
+# for i in range(len(list)):
+#     for j in range(i+1,len(list)):
+#         for k in range(j+1,len(list)):
+#           if list[i]+list[j]+list[k]==target:
+#             print("These are Values",list[i],list[j],list[k])
+#             print("These are the Indexes",i,j,k)
+
+
+
+
+
