d4a2ddbd9038e130cab382889a8eee12afa1062d
Yash Thawkar <yashthawkar595@gmail.com>
2026-09-01 07:57:56 +0530

    Abstraction_in_Online_Food

---
+++ abstraction_task4.py
@@ -0,0 +1,30 @@
+from abc import ABC
+class Online_food(ABC):
+    def select_item(self):
+        pass
+    def Bill(self):
+        pass
+    def Success(self):
+        pass
+class Zomato(Online_food):
+    def select_item(self,Your_choice):
+        print("Your Cart:",Your_choice)
+    def Bill(self,Amount):
+        print("Your Total Bill is",Amount)
+    def Success(self,Your_choice,Amount):
+        print("Your Order of",Your_choice,"of Amount",Amount,"is Placed Successfully")
+class Swiggy(Online_food):
+    def select_item(self,Your_choice):
+        print("Your Cart:",Your_choice)
+    def Bill(self,Amount):
+        print("Your Total Bill is",Amount)
+    def Success(self,Your_choice,Amount):
+        print("Your Order of",Your_choice,"of Amount",Amount,"is Placed Successfully")
+app_zomato=Zomato()
+app_zomato.select_item("Muttor Panir,Tawa Roti")
+app_zomato.Bill(247.00)
+app_zomato.Success("Muttor Panir,Tawa Roti",247.00)
+app_swiggy=Swiggy()
+app_swiggy.select_item("Mutton Biryani")
+app_swiggy.Bill(349.00)
+app_swiggy.Success("Mutton Biryani",349.00)
