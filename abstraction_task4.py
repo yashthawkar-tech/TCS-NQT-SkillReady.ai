88f3203101d14e741a3d5d017af05f008fffc408
Yash Thawkar <yashthawkar595@gmail.com>
2026-09-02 23:55:11 +0530

    Add Tooing class and update order success calls

--- abstraction_task4.py
+++ abstraction_task4.py
@@ -20,6 +20,13 @@ class Swiggy(Online_food):
         print("Your Total Bill is",Amount)
     def Success(self,Your_choice,Amount):
         print("Your Order of",Your_choice,"of Amount",Amount,"is Placed Successfully")
+class Tooing(Online_food):
+    def select_item(self,Your_choice):
+        print("Your Cart:",Your_choice)
+    def Bill(self,Amount):
+        print("Your Total Bill is",Amount)
+    def Success(self,Your_choice,Amount):
+        print("Your Order of",Your_choice,"of Amount",Amount,"is Placed Successfully")
 app_zomato=Zomato()
 app_zomato.select_item("Muttor Panir,Tawa Roti")
 app_zomato.Bill(247.00)
@@ -28,3 +35,7 @@ app_swiggy=Swiggy()
 app_swiggy.select_item("Mutton Biryani")
 app_swiggy.Bill(349.00)
 app_swiggy.Success("Mutton Biryani",349.00)
+app_tooing=Tooing()
+app_tooing.select_item("Dosa,Idli")
+app_tooing.Bill(328.00)
+app_tooing.Success=("Dosa,Idli",328.00)
