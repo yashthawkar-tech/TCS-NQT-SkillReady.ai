e8d2110d0a797025626fabf17d25767aae84d702
Yash Thawkar <yashthawkar595@gmail.com>
2026-09-05 21:46:50 +0530

    Add pincode to student dictionary
    Added 'pincode' key to student dictionary.

--- dict_2.py
+++ dict_2.py
@@ -1,11 +1,14 @@
 student={
     "name":"Aakash",
     "age" :28,
-    "city" :"Tirora"
+    "city" :"Tirora",
+    "pincode":441911
 }
 print(student["name"])
 print(student["age"])
 print(student["city"])
+print(student["pincode"])
+
 
 #it will print a key Value Assigned to that Particular Variable
 
